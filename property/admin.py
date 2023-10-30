f7073290e567bf1e2a0cfde371991c7a5a3e8b84
Maxim <38497831+StableBig@users.noreply.github.com>
2023-10-30 05:51:35 +0300

    Add new fields and models

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,57 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint, Owner
 
 
-admin.site.register(Flat)
+class FlatInstanceInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ('owner',)
+
+
+@admin.register(Flat)
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ('town', 'address')
+    readonly_fields = ('created_at',)
+    list_display = (
+        'address',
+        'price',
+        'new_building',
+        'construction_year',
+        'town',
+        )
+    list_editable = ('new_building',)
+    list_filter = (
+        'new_building',
+        'rooms_number',
+        'has_balcony',
+        'active',
+        )
+    raw_id_fields = (
+        'liked_by',
+        )
+    inlines = [FlatInstanceInline]
+
+
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'flat',
+        'text',
+        )
+    raw_id_fields = (
+        'author',
+        'flat',
+        )
+
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'phonenumber',
+        'pure_phone',
+        )
+    raw_id_fields = (
+        'flats',
+        )
--- property/models.py
+++ property/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    new_building = models.BooleanField('Новостройка', null=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -34,18 +35,72 @@ class Flat(models.Model):
         'Количество комнат в квартире',
         db_index=True)
     living_area = models.IntegerField(
-        'количество жилых кв.метров',
+        'Количество жилых кв.метров',
         null=True,
         blank=True,
         db_index=True)
 
     has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
-    active = models.BooleanField('Активно-ли объявление', db_index=True)
+    active = models.BooleanField('Активно ли объявление', db_index=True)
     construction_year = models.IntegerField(
         'Год постройки здания',
         null=True,
         blank=True,
         db_index=True)
 
+    liked_by = models.ManyToManyField(User, verbose_name='Кто лайкнул',
+                                      related_name='liked_flats',
+                                      blank=True
+                                      )
+
+    class Meta:
+        verbose_name = 'Квартиру'
+        verbose_name_plural = 'Квартиры'
+
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Complaint(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Кто жаловался',
+        related_name='complaints'
+    )
+    flat = models.ForeignKey(
+        Flat,
+        on_delete=models.CASCADE,
+        verbose_name='Квартира, на которую пожаловались',
+        related_name='complaints'
+    )
+    text = models.TextField(
+        'Текст жалобы'
+    )
+
+    class Meta:
+        verbose_name = 'Жалобу'
+        verbose_name_plural = 'Жалобы'
+
+    def __str__(self):
+        return self.complaint_text
+
+
+class Owner(models.Model):
+    name = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    phonenumber = models.CharField('Номер телефона владельца', max_length=20)
+    pure_phone = PhoneNumberField('Нормализованный номер телефона владельца',
+                                  region="RU", null=True, blank=True,
+                                  )
+    flats = models.ManyToManyField(Flat, related_name='owners',
+                                   verbose_name='Квартиры в собственности',
+                                   db_index=True
+                                   )
+
+    class Meta:
+        verbose_name = 'Собственника'
+        verbose_name_plural = 'Собственники'
+
+    def __str__(self):
+        return self.name
